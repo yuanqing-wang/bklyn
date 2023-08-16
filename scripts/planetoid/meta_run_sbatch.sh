8ba614073af1812d03865b3e883ab52fac918de5
Yuanqing Wang <wangyq+gh@wangyq.net>
2023-08-16 00:29:19 -0400

    Initialize fast

---
+++ scripts/planetoid/meta_run_sbatch.sh
@@ -0,0 +1,43 @@
+for hidden_features in 32; do
+for depth in 2; do
+for learning_rate in 1e-2; do
+for weight_decay in 1e-3; do
+for gamma in 0.7; do
+for dropout in 0.4 0.5 0.6; do
+for edge_drop in 0.2; do
+for embedding_features in 4 8; do
+for patience in 5; do
+for factor in 0.5; do
+
+sbatch \
+	--nodes=1 \
+	--ntasks-per-node=1 \
+	--cpus-per-task=1 \
+	--time=00:30:00 \
+	--mem=5GB \
+	--job-name=aperol \
+    --gres=gpu:1 \
+	--output=%A.out \
+    --wrap "python run.py \
+    --hidden_features $hidden_features \
+    --learning_rate $learning_rate \
+    --depth $depth \
+    --weight_decay $weight_decay \
+    --gamma $gamma \
+    --dropout $dropout \
+    --edge_drop $edge_drop \
+    --embedding_features $embedding_features \
+    --factor $factor \
+    --patience $patience\
+"
+
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
